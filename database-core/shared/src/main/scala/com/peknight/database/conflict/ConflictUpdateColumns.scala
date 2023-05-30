f0cdc8197f75719e96b4c65f550473918d5f0869
peknight <JKpeknight@gmail.com>
2023-05-30 10:45:03 +0800

    update

--- database-core/shared/src/main/scala/com/peknight/database/conflict/ConflictUpdateColumns.scala
+++ database-core/shared/src/main/scala/com/peknight/database/conflict/ConflictUpdateColumns.scala
@@ -3,3 +3,6 @@ package com.peknight.database.conflict
 trait ConflictUpdateColumns[A]:
   def columns: Seq[String]
 end ConflictUpdateColumns
+object ConflictUpdateColumns:
+  def apply[A](using conflictUpdateColumns: ConflictUpdateColumns[A]): ConflictUpdateColumns[A] = conflictUpdateColumns
+end ConflictUpdateColumns
--- database-core/shared/src/main/scala/com/peknight/database/conflict/IndexColumnNames.scala
+++ database-core/shared/src/main/scala/com/peknight/database/conflict/IndexColumnNames.scala
@@ -3,3 +3,6 @@ package com.peknight.database.conflict
 trait IndexColumnNames[A] extends ConflictTarget[A]:
   def columns: Seq[String]
 end IndexColumnNames
+object IndexColumnNames:
+  def apply[A](using indexColumnNames: IndexColumnNames[A]): IndexColumnNames[A] = indexColumnNames
+end IndexColumnNames
